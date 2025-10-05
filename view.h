47737128d091060140fc4ea03ab3c35c872118be
Naveen Kumar T R <trnnaveen630@gmail.com>
2025-10-05 09:13:50 +0530

    Create view.h

---
+++ view.h
@@ -0,0 +1,13 @@
+/* Name : NAVEEN KUMAR T R
+   Date : 26/09/2025*/
+   
+#ifndef VIEW_H
+#define VIEW_H
+#include<stdlib.h>
+
+
+void read_validate_view_arg(char **argv);//in view validate the file mp3 decleration.
+
+void print(char *);//function decleration to print.
+
+#endif
