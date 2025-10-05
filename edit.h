3f4020e251a246329d0df05b1d5faa49cc3b30e9
Naveen Kumar T R <trnnaveen630@gmail.com>
2025-10-05 09:10:35 +0530

    Create edit.h

---
+++ edit.h
@@ -0,0 +1,12 @@
+/* Name : NAVEEN KUMAR T R
+   Date : 26/09/2025*/
+   
+#ifndef EDIT_H
+#define EDIT_H
+#include<stdlib.h>
+
+void edit_arg(char **);//edit funcation declaration.
+
+void check_tag(char *, char *);//cheak tag funcation declaration.
+
+#endif
